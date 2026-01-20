4ccf7ebfa5a541773de444dfe7b4b094a0731b26
Abimbola 'boe' Esan <boesif@outlook.com>
2026-01-19 18:49:27 -0800

    Initial commit: Portfolio website files

---
+++ Notebooks/hello_world.py
@@ -0,0 +1,19 @@
+# Sprint 5: Software Developlment Tools
+# Chapter 4: Git and GitHub
+# Lesson 6: Branches
+
+import random
+
+greetings = [
+    "Hello, Git!",
+    "Greetings, developer!",
+    "Welcome to branching!",
+    "Hi there, coding friend!",
+    "Happy coding!"
+]
+
+def get_random_greeting():
+    return random.choice(greetings)
+
+print(get_random_greeting())
+print("Learning about branches today!")
